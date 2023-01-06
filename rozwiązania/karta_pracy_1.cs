7a541ac9151e5cc3f36cb11603232a00891ba2aa
czareq089 <czarek.kotlarski@o2.pl>
2023-01-06 16:16:02 +0100

    dodanie karty pracy 1

---
+++ rozwiązania/karta_pracy_1.cs
@@ -0,0 +1,41 @@
+﻿double a = int.Parse(Console.ReadLine());
+double b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+
+//1 
+Console.WriteLine("Zadanie 1");
+
+Console.WriteLine((a*a)+(b*b));
+
+//2
+Console.WriteLine("Zadanie 2");
+
+Console.WriteLine(Math.Pow((a+b), 2));
+
+//3
+Console.WriteLine("Zadanie 3");
+
+Console.WriteLine(Math.Pow((a-b), 3));
+
+//4
+Console.WriteLine("Zadanie 4");
+
+Console.WriteLine(a * b * c);
+
+//5
+Console.WriteLine("Zadanie 5");
+
+Console.WriteLine(2*(a+b)/5);
+Console.WriteLine((a + b) * 0.4);
+
+//6
+Console.WriteLine("Zadanie 6");
+
+double brutto = int.Parse((Console.ReadLine()));
+Console.WriteLine(brutto / 1.23);
+
+//7
+Console.WriteLine("Zadanie 7");
+
+Console.WriteLine(a%b);
